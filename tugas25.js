c37ed7162c2aa40e3fcbec582bed0a3ddfeed9bb
ADI-Netizen <71365604+ADI-Netizen@users.noreply.github.com>
2020-09-22 08:12:19 +0700

    Add files via upload

---
+++ tugas25.js
@@ -0,0 +1,14 @@
+function dualima() {
+    var angka = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]
+    console.log("Sebelumnya : ", angka.toString())
+    angka = angka.sort()
+    console.log("Ascending : ", angka.toString())
+    angka = angka.reverse()
+    console.log("Descending : ", angka.toString())
+    var dl = angka.filter(function (no) {
+        return no > 10;
+    });
+    console.log("Filter > : ", dl.toString())
+}
+
+dualima()
